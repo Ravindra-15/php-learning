ea35a2c0071e11ba68600bdb2e61ecb2883cff48
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-09-25 19:46:16 +0530

    Operators

--- arithmeticOperator.php
+++ arithmeticOperator.php
@@ -3,16 +3,19 @@ $num1 = 10;
 $num2 = 20;
 
 echo "Sum is: " . ($num1 + $num2);
-echo '<br>';  // Corrected to <br>
+echo '<br>';   
 echo "Subtraction is: " . ($num1 - $num2);
 echo '<br>';
 echo "Multiplication is: " . ($num1 * $num2);
 echo '<br>';
 echo "Division is: " . ($num1 / $num2);
 echo '<br>';
-echo "Modulo is: " . ($num2 % $num1);  // Corrected order for modulus: $num2 % $num1
+echo "Modulo is: " . ($num2 % $num1);   
 echo '<br>';
-
 echo "Power (num2 raised to num1) is: " . ($num2 ** $num1);  // Exponentiation
 echo '<br>';
+
+const num3 = 10;
+const num4 = 20;
+echo num3 + num4;
 ?>
---
+++ assignmentOp.php
@@ -0,0 +1,36 @@
+<?php 
+$num1 = "4";
+$num2  = "5";
+echo "The value of num1 is:".($num1);
+echo "<br>";
+echo "The value of num2 is:".($num2);
+echo "<br>";
+
+// $num1 = $num2;
+// echo "The value of num1 after assigning num2 value is: ".($num1);
+// echo "<br>";
+// echo "Now the value of num1 is:".($num1);
+// echo "<br>";
+// echo "And the value of num2 is:".($num2);
+// echo "<br>";
+
+// $num1  = $num1 + $num2;
+/*$num1 += $num2;
+echo "The value is:".($num1);
+echo "<br>";*/
+
+/*$num2 -= $num1;
+echo "The value is:".($num2);
+echo "<br>";*/
+
+/*$num1 *= $num2;
+echo "The value is:".($num1);
+echo "<br>";*/
+
+/*$num2 /= $num1;
+echo "The value is:".($num2);
+echo "<br>";*/
+
+
+
+?>
