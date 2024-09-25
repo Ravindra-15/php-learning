8bd5899e881ef11693af4064f5f234842309d931
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-09-26 00:01:20 +0530

    Operators

---
+++ stringOperator.php
@@ -0,0 +1,15 @@
+<?php
+
+$str1 = "Hello Coders! ";
+$str2 = "This is Ravindra Kumar";
+
+//...............................Concatenate Operator(.)..............................
+echo $str1.$str2;
+echo "<br>";
+$str1 .= $str2;
+echo $str1;
+
+//............................using variable with string........................
+echo "<br>";
+echo "Variable with string ".$str1;
+?>
