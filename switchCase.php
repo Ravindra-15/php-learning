7899782a7659c5d90db6bb88c2781cda181fee28
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-09-26 01:15:17 +0530

    Conditional Statemet

---
+++ switchCase.php
@@ -0,0 +1,26 @@
+<?php
+
+$favoriteColor = "blue";
+
+switch($favoriteColor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    case "yellow":
+        echo "Your favorite color is yellow!";
+        break;
+    case "purple":
+        echo "Your favorite color is purple!";
+        break;
+    default:
+        echo "I don't know your favorite color!";
+        break;
+}
+
+?>
